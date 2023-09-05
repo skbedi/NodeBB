0c294f27eaae41a96927a1aa7cb226c569ad1df1
Simran Bedi <simranbedi@macbook-pro-424.wifi.local.cmu.edu>
2023-09-05 13:24:33 -0400

    half errors solved

--- src/topics/bookmarks.js
+++ src/topics/bookmarks.js
@@ -1,66 +1,89 @@
-
-'use strict';
-
-const async = require('async');
-
-const db = require('../database');
-const user = require('../user');
-
-module.exports = function (Topics) {
-    Topics.getUserBookmark = async function (tid, uid) {
-        if (parseInt(uid, 10) <= 0) {
-            return null;
-        }
-        return await db.sortedSetScore(`tid:${tid}:bookmarks`, uid);
-    };
-
-    Topics.getUserBookmarks = async function (tids, uid) {
-        if (parseInt(uid, 10) <= 0) {
-            return tids.map(() => null);
-        }
-        return await db.sortedSetsScore(tids.map(tid => `tid:${tid}:bookmarks`), uid);
-    };
-
-    Topics.setUserBookmark = async function (tid, uid, index) {
-        await db.sortedSetAdd(`tid:${tid}:bookmarks`, index, uid);
-    };
-
-    Topics.getTopicBookmarks = async function (tid) {
-        return await db.getSortedSetRangeWithScores(`tid:${tid}:bookmarks`, 0, -1);
-    };
-
-    Topics.updateTopicBookmarks = async function (tid, pids) {
-        const maxIndex = await Topics.getPostCount(tid);
-        const indices = await db.sortedSetRanks(`tid:${tid}:posts`, pids);
-        const postIndices = indices.map(i => (i === null ? 0 : i + 1));
-        const minIndex = Math.min(...postIndices);
-
-        const bookmarks = await Topics.getTopicBookmarks(tid);
-
-        const uidData = bookmarks.map(b => ({ uid: b.value, bookmark: parseInt(b.score, 10) }))
-            .filter(data => data.bookmark >= minIndex);
-
-        await async.eachLimit(uidData, 50, async (data) => {
-            let bookmark = Math.min(data.bookmark, maxIndex);
-
-            postIndices.forEach((i) => {
-                if (i < data.bookmark) {
-                    bookmark -= 1;
-                }
-            });
-
-            // make sure the bookmark is valid if we removed the last post
-            bookmark = Math.min(bookmark, maxIndex - pids.length);
-            if (bookmark === data.bookmark) {
-                return;
-            }
-
-            const settings = await user.getSettings(data.uid);
-            if (settings.topicPostSort === 'most_votes') {
-                return;
-            }
-
-            await Topics.setUserBookmark(tid, data.uid, bookmark);
-        });
-    };
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// The next line calls a function in a module that has not been updated to TS yet
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+const async_1 = __importDefault(require("async"));
+// The next line calls a function in a module that has not been updated to TS yet
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+const database_1 = __importDefault(require("../database"));
+// The next line calls a function in a module that has not been updated to TS yet
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+const user_1 = __importDefault(require("../user"));
+class Topics {
+    getUserBookmark(tid, uid) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (parseInt(uid, 10) <= 0) {
+                return null;
+            }
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            return yield database_1.default.sortedSetScore(`tid:${tid}:bookmarks`, uid);
+        });
+    }
+    getUserBookmarks(tids, uid) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (parseInt(uid, 10) <= 0) {
+                return tids.map(() => null);
+            }
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            return yield database_1.default.sortedSetsScore(tids.map(tid => `tid:${tid}:bookmarks`), uid);
+        });
+    }
+    setUserBookmark(tid, uid, index) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield database_1.default.sortedSetAdd(`tid:${tid}:bookmarks`, index, uid);
+        });
+    }
+    getTopicBookmarks(tid) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            return yield database_1.default.getSortedSetRangeWithScores(`tid:${tid}:bookmarks`, 0, -1);
+        });
+    }
+    updateTopicBookmarks(tid, pids) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const maxIndex = yield Topics.getPostCount(tid);
+            const indices = yield database_1.default.sortedSetRanks(`tid:${tid}:posts`, pids);
+            const postIndices = indices.map((i) => (i === null ? 0 : i + 1));
+            const minIndex = Math.min(...postIndices);
+            const bookmarks = yield Topics.getTopicBookmarks(tid);
+            const uidData = bookmarks.map(b => ({ uid: b.value, bookmark: parseInt(b.score, 10) }))
+                .filter(data => data.bookmark >= minIndex);
+            yield async_1.default.eachLimit(uidData, 50, (data) => __awaiter(this, void 0, void 0, function* () {
+                let bookmark = Math.min(data.bookmark, maxIndex);
+                postIndices.forEach((i) => {
+                    if (i < data.bookmark) {
+                        bookmark -= 1;
+                    }
+                });
+                // make sure the bookmark is valid if we removed the last post
+                bookmark = Math.min(bookmark, maxIndex - pids.length);
+                if (bookmark === data.bookmark) {
+                    return;
+                }
+                const settings = yield user_1.default.getSettings(data.uid);
+                if (settings.topicPostSort === 'most_votes') {
+                    return;
+                }
+                yield Topics.setUserBookmark(tid, data.uid, bookmark);
+            }));
+        });
+    }
+}
+;
---
+++ src/topics/bookmarks.ts
@@ -0,0 +1,86 @@
+
+
+// The next line calls a function in a module that has not been updated to TS yet
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+import async from 'async';
+
+// The next line calls a function in a module that has not been updated to TS yet
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+import db from '../database';
+
+// The next line calls a function in a module that has not been updated to TS yet
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+import user from '../user';
+
+
+export async function (Topics) {
+    async getUserBookmark(tid : number, uid : string) {
+        if (parseInt(uid, 10) <= 0) {
+            return null;
+        }
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        return await db.sortedSetScore(`tid:${tid}:bookmarks`, uid);
+    }
+
+    async  getUserBookmarks(tids: number[], uid: string) {
+        if (parseInt(uid, 10) <= 0) {
+            return tids.map(() => null);
+        }
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        return await db.sortedSetsScore(tids.map(tid => `tid:${tid}:bookmarks`), uid);
+    }
+
+    async  setUserBookmark(tid : number, uid : string, index : string) {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await db.sortedSetAdd(`tid:${tid}:bookmarks`, index, uid);
+    }
+
+    async  getTopicBookmarks(tid : number) {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        return await db.getSortedSetRangeWithScores(`tid:${tid}:bookmarks`, 0, -1);
+    }
+
+    async  updateTopicBookmarks(tid : number, pids : number[]) {
+        const maxIndex: number = await Topics.getPostCount(tid);
+        const indices: number[] = await db.sortedSetRanks(`tid:${tid}:posts`, pids);
+        const postIndices: number[] = indices.map((i: number) => (i === null ? 0 : i + 1));
+        const minIndex: number = Math.min(...postIndices);
+
+        //const bookmarks = await Topics.getTopicBookmarks(tid);
+
+        const uidData = bookmarks.map(b => ({ uid: b.value, bookmark: parseInt(b.score, 10) }))
+            .filter(data => data.bookmark >= minIndex);
+            type CustomData = {
+                bookmark : number,
+                uid : number
+            }
+
+            await async.eachLimit(uidData, 50, async (data:CustomData) => {
+               let bookmark = Math.min(data.bookmark, maxIndex);
+
+                postIndices.forEach((i) => {
+                    if (i < data.bookmark) {
+                        bookmark -= 1;
+                    }
+                });
+
+                // make sure the bookmark is valid if we removed the last post
+                bookmark = Math.min(bookmark, maxIndex - pids.length);
+                if (bookmark === data.bookmark) {
+                    return;
+                }
+
+                const settings = await user.getSettings(data.uid);
+                if (settings.topicPostSort === 'most_votes') {
+                    return;
+                }
+
+                await Topics.setUserBookmark(tid, data.uid, bookmark);
+            });
+    }
+};
+export default Topics;
